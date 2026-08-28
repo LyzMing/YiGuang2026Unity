d319f0c1d851a6ff25e7840a20ab4891cf86ad83
3460797530 <3460797530@qq.com>
2026-08-28 10:27:26 +0800

    添加跑步和跳跃动画；角色转向时会左右翻转了

--- YiGuang2026/Assets/Scripts/PlayerMove.cs
+++ YiGuang2026/Assets/Scripts/PlayerMove.cs
@@ -9,11 +9,15 @@ public class PlayerMove : MonoBehaviour
 
     private Rigidbody2D rb;
     private float moveController;
+    private bool isRunScript;
+    private bool isJumpScript;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +25,20 @@ public class PlayerMove : MonoBehaviour
     {
         moveController = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveSpeed * moveController, rb.velocity.y);
+        if (moveController > 0)
+            transform.localScale = new Vector2(1, 1);
+        if (moveController < 0)
+            transform.localScale = new Vector2(-1, 1);
 
         if (Input.GetButtonDown("Jump"))
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        if (rb.velocity.y > 0.3f)
+            isJumpScript = true;
+        else
+            isJumpScript = false;
+        anim.SetBool("isJump", isJumpScript);
+
+        isRunScript = (moveController != 0);
+        anim.SetBool("isRun", isRunScript);
     }
 }
