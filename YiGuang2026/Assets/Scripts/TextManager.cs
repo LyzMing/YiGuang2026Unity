14241c56480cdedddcdd96f8750e404f13541a52
3460797530 <3460797530@qq.com>
2026-09-02 19:08:27 +0800

    做了拾取物品加分

---
+++ YiGuang2026/Assets/Scripts/TextManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private GameState gameState;
+
+    private string score = "0";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = gameState.score.ToString();
+        scoreText.text = "Score: " + score;
+    }
+}
