b43a450a0afdc0aa2be1d5ab8e2b820eaef6a66f
3460797530 <3460797530@qq.com>
2026-09-02 16:43:25 +0800

    写了土狼时间，优化跳跃手感

--- YiGuang2026/Assets/Scripts/GroundChecker.cs
+++ YiGuang2026/Assets/Scripts/GroundChecker.cs
@@ -6,6 +6,8 @@ public class GroundChecker : MonoBehaviour
 {
     public bool isGround;
 
+    private Coroutine coyoteCoroutine;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +23,30 @@ public class GroundChecker : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
             isGround = true;
+            if (coyoteCoroutine != null)
+            {
+                StopCoroutine(coyoteCoroutine);
+                coyoteCoroutine = null;
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-            isGround = false;
+        {
+            if (isActiveAndEnabled)
+                coyoteCoroutine = StartCoroutine(CoyoteTime());
+        }
+    }
+
+    //优化跳跃手感的土狼时间
+    private IEnumerator CoyoteTime()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isGround = false;
+        coyoteCoroutine = null;
     }
 }
